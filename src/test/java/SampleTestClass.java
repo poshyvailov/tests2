008adde2651937f386ff5593aea1aac8257f19ca
poshyvailov <pelmel12@gmail.com>
2020-06-19 20:21:27 +0300

    Test

--- src/test/java/SampleTestClass.java
+++ src/test/java/SampleTestClass.java
@@ -1,10 +1,15 @@
+import org.junit.Before;
 import org.junit.Test;
 
 public class SampleTestClass {
+
+    @Before
 
     @Test
     public void sampleTestMethod(){
         assert 1 == 1;
     }
+
+
 
 }
