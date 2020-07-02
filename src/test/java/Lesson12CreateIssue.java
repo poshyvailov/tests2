7afd7626de1685861567764ecd8f0d6383557390
poshyvailov <pelmel12@gmail.com>
2020-07-02 15:00:24 +0300

    Test update

--- src/test/java/Lesson12CreateIssue.java
+++ src/test/java/Lesson12CreateIssue.java
@@ -1,27 +1,90 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import java.time.Duration;
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.testng.AssertJUnit.assertEquals;
+
 
 public class Lesson12CreateIssue {
 
+
+
     WebDriver driver = null;
+
 
     @BeforeMethod
     public void setUp() {
         WebDriverFactory.createInstance("Chrome");
         driver = WebDriverFactory.getDriver();
         driver.manage().window().maximize();
+
     }
 
     @Test
-    public void createIssueSteps(){
-        driver.get("https://www.deezer.com/ru/");
+    public void createIssueSteps() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15).getSeconds());
+
+        driver.get("https://jira.hillel.it/secure/Dashboard.jspa");
+        driver.findElement(By.id("login-form-username")).sendKeys("poshyvailov");
+        driver.findElement(By.id("login-form-password")).sendKeys("poshyvailov");
+        driver.findElement(By.id("login")).click();
+        wait.until(presenceOfElementLocated(By.xpath("//*[@id='dashboard-content']//h1"))).isDisplayed();
+
+        // без тред слип не нажимается кнопка Create
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Находим и нажимаем на кнопку Create
+        driver.findElement(By.id("create_link")).click();
+        wait.until(presenceOfElementLocated(By.xpath("//h2[@title='Create Issue']"))).isDisplayed();
+
+
+        //Ищем поле Project, чистим его, вводим наши данные, нажимаем табуляцию
+        driver.findElement(By.id("project-field")).clear();
+        driver.findElement(By.id("project-field")).sendKeys("Webinar (WEBINAR)");
+        driver.findElement(By.id("project-field")).sendKeys(Keys.TAB);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Ищем поле Issue Type, чистим его, вводим наши данные, нажимаем табуляцию
+        driver.findElement(By.id("issuetype-field")).clear();
+        driver.findElement(By.id("issuetype-field")).sendKeys("Story");
+        driver.findElement(By.id("issuetype-field")).sendKeys(Keys.TAB);
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Заполняем поле с саммари и проверяем,что наш юзер нейм есть в поле Reporter
+        driver.findElement(By.id("summary")).sendKeys("Some New Ticket For Test");
+        wait.until(presenceOfElementLocated(By.xpath("//optgroup[@id='reporter-group-suggested']//option[@value='poshyvailov']"))).isDisplayed();
+
+        //Нажимаем на кнопку для создания тикета, проверяем,что появился поп-ап с сообщением о создании ти кета
+        driver.findElement(By.id("create-issue-submit")).click();
+        boolean checkIfPopUpIsAppeared = wait.until(presenceOfElementLocated(By.id("aui-flag-container"))).isDisplayed();
+        assertEquals(checkIfPopUpIsAppeared, true);
+
     }
 
 
+
+
     @AfterMethod
-    public void tearDown(){
-        driver.quit();
+    public void tearDown() {
+       // driver.quit();
     }
 }
