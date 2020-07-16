c4bbce14512df9fa954c0d5c4cc93ac70191dd0e
poshyvailov <pelmel12@gmail.com>
2020-07-16 10:03:51 +0300

    testng.xml edited

--- src/main/java/TestNGListener.java
+++ src/main/java/TestNGListener.java
@@ -16,7 +16,7 @@ public class TestNGListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult result) {
         String browserName = result.getTestContext().getCurrentXmlTest().getParameter("browserName");
-        System.out.println("Browser name from listener is " + browserName);
+        //System.out.println("Browser name from listener is " + browserName);
         WebDriverFactory.createInstance(browserName);
     }
 
