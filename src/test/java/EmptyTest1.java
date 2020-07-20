578207f2f5b7f738e5e5d1bb9bb59595b1a3f7d9
poshyvailov <pelmel12@gmail.com>
2020-07-20 09:59:23 +0300

    Added two empty tests

--- src/test/java/EmptyTest1.java
+++ src/test/java/EmptyTest1.java
@@ -2,24 +2,24 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class emptyTest1 {
+public class EmptyTest1 {
 
     @BeforeMethod
     public void setUpSteps(){
-        System.out.println("BeforeMethod from emptyTest1 is working now");
+        System.out.println("BeforeMethod from EmptyTest1 is working now");
 
     }
 
     @Test
     public void emptyTestSteps(){
         assert 1==1;
-        System.out.println("Test method from emptyTest1 is working now");
+        System.out.println("Test method from EmptyTest1 is working now");
 
     }
 
     @AfterMethod
     public void tearDown(){
-        System.out.println("AfterMethod from emptyTest1 is working now");
+        System.out.println("AfterMethod from EmptyTest1 is working now");
 
     }
 }
--- src/test/java/EmptyTest2.java
+++ src/test/java/EmptyTest2.java
@@ -3,25 +3,25 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 
-public class emptyTest2 {
+public class EmptyTest2 {
 
     @BeforeMethod
     public void setUpSteps(){
-        System.out.println("BeforeMethod from emptyTest2 is working now");
+        System.out.println("BeforeMethod from EmptyTest2 is working now");
 
     }
 
     @Test
     public void emptyTestSteps(){
         assert 1!=0;
-        System.out.println("Test method from emptyTest2 is working now");
+        System.out.println("Test method from EmptyTest2 is working now");
 
 
     }
 
     @AfterMethod
     public void tearDown(){
-        System.out.println("AfterMethod from emptyTest2 is working now");
+        System.out.println("AfterMethod from EmptyTest2 is working now");
 
     }
 }
